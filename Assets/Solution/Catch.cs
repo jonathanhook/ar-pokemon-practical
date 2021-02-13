7fc777c3461cdc29c3f4f9aba6143f4ac246d4cf
Jonathan Hook <jonathan.hook+github@gmail.com>
2021-02-13 13:35:50 +0000

    With solution for release

---
+++ Assets/Solution/Catch.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Catch : MonoBehaviour
+{
+	void OnCollisionEnter(Collision c)
+	{
+		if (c.gameObject.name == "MewPokemon")
+		{
+			c.gameObject.SetActive(false);
+			transform.gameObject.SetActive(false);
+		}
+	}
+}
